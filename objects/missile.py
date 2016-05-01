4d2de6f66a45262ca2f2e99a124f077a256ce330
Mary Connolly <mconnol6@nd.edu>
2016-05-01 15:23:15 -0400

    client_gs adds data to deferred queue when new missile or bomb created

--- objects/missile.py
+++ objects/missile.py
@@ -20,8 +20,8 @@ class Missile(pygame.sprite.Sprite):
 		self.dx = -1 * v * cos(angle)
 		self.dy = -1 * v * sin(angle)
 
-	def __getstate__(self):
-		return {"pos" : self.pos, "start": self.start, "fx": self.fx, "fy": self.fy, "da": self.da, "dx": self.dx, "dy": self.dy}
+#	def __getstate__(self):
+#		return {"pos" : self.pos, "start": self.start, "fx": self.fx, "fy": self.fy, "da": self.da, "dx": self.dx, "dy": self.dy}
 
 	def tick(self):
 
@@ -45,8 +45,8 @@ class Bomb(Missile):
 
 		self.dest = dest #destination (0-8) of bomb
 
-	def __getstate__(self):
-		return {"pos" : self.pos, "start": self.start, "fx": self.fx, "fy": self.fy, "da": self.da, "dx": self.dx, "dy": self.dy, "dest": self.dest}
+#	def __getstate__(self):
+#		return {"pos" : self.pos, "start": self.start, "fx": self.fx, "fy": self.fy, "da": self.da, "dx": self.dx, "dy": self.dy, "dest": self.dest}
 
 	def tick(self):
 
