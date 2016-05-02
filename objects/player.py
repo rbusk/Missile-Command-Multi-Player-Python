e59fbeb1cd69c55034704c5857420d5a2963b35f
Ryan <rctbusk@gmail.com>
2016-05-01 22:38:15 -0400

    added heavy comments

--- objects/player.py
+++ objects/player.py
@@ -7,57 +7,65 @@ from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
 from client_gs import *
 
-TYPE = None
-PLAYER_1 = 40003
-PLAYER_2 = 40008
+TYPE = None #Bomb or Missile
+PLAYER_1 = 40003 #port for player 1
+PLAYER_2 = 40008 #port for player 2
 HOST = '127.0.0.1'
 #HOST = '10.176.31.106'
 
-gs = Gamespace()
+gs = Gamespace() #initialize local gamespace
 
 class Player(Protocol):
 	def dataReceived(self,data):
-		if data == 'Missiles' or data == 'Bombs':
-			self.TYPE = data
-			gs.TYPE = self.TYPE
-			self.lc = LoopingCall(gs.ticks)
-			self.lc.start(1./60)
-		elif data == "Turn Over":
-			gs.reset_turn()
-		elif data == "Round Over":
-			print data
-			gs.reset_round()
-		elif data == "Game Over":
+		"""when data is received, parse data and send it to the right place"""
+		if data == 'Missiles' or data == 'Bombs': #check if initial send is bomb or missile
+			self.TYPE = data #set type
+			gs.TYPE = self.TYPE #set type in gamespace
+			self.lc = LoopingCall(gs.ticks) #start looping call
+			self.lc.start(1./60) #set clock time
+		elif data == "Turn Over": #if server says turn is over
+			gs.reset_turn() #reset turn in gs
+		elif data == "Round Over": #same with round
+			gs.reset_round() #reset round
+		elif data == "Game Over": #same wih game
 			gs.game_over()
 		else:
-			data_queue.put(data)
+			data_queue.put(data) #send any other data to queue
 
 
 	def connectionMade(self):
-		command_queue.get().addCallback(self.callback)
-		data_queue.get().addCallback(gs.callback)
+		"""when initial connection is made. add callback queue for both queues"""
+		command_queue.get().addCallback(self.callback) #command queue
+		data_queue.get().addCallback(gs.callback) #data queue
 
 	def connectionLost(self,reason):
+		"""stop reactor when connection is lost"""
 		reactor.stop()
 
 	def callback(self,data):
+		"""callback to add to the queue"""
 		self.transport.write(data)
 		command_queue.get().addCallback(self.callback)
 
 
 class ClientConnFactory(ClientFactory):
 	def buildProtocol(self,addr):
+		"""return player when factory is called"""
 		return Player()
 
 if __name__ == '__main__':
+	"""run this to start game"""
 	for args in sys.argv:
 		if args == "1":
+			#if first player, run this
 			PLAYER = 1
 			reactor.connectTCP(HOST,PLAYER_1,ClientConnFactory())
 			break
 		if args == "2":
+			#if second player, run this
 			PLAYER = 2
 			reactor.connectTCP(HOST,PLAYER_2,ClientConnFactory())
 			break
+	#run reactor
 	reactor.run()
 
--- objects/server.py
+++ objects/server.py
@@ -8,12 +8,13 @@ PLAYER_1_PORT = 40003
 PLAYER_2_PORT = 40008
 global NUMBER_OF_PLAYERS
 NUMBER_OF_PLAYERS = 0
+#flags to check if round is over for both players
 round_over = 0
 turn_over = 0
 game_over = 0
 
-p1_data_queue = DeferredQueue()
-p2_data_queue = DeferredQueue()
+p1_data_queue = DeferredQueue() #server queue from player 1 to player 2
+p2_data_queue = DeferredQueue() #server queue from player 2 to player 1
 
 
 #Player 1
@@ -22,33 +23,39 @@ class Player1Conn(Protocol):
 		self.addr = addr
 
 	def dataReceived(self,data):
-		global round_over, turn_over, game_over
+		"""parse data received from client"""
+		global round_over, turn_over, game_over #global variables to share between players
 		if data == "Round Over":
+			"""only sends round over if both players complete round"""
 			round_over += 1
 			if round_over == 2:
 				p1_data_queue.put("Round Over")
 				p2_data_queue.put("Round Over")
 				round_over = 0
 		elif data == "Turn Over":
+			"""only sends turn over if both players complete turn"""
 			turn_over += 1
 			if turn_over == 2:
 				p1_data_queue.put("Turn Over")
 				p2_data_queue.put("Turn Over")
 				turn_over = 0
 		elif data == "Game Over":
+			"""only sends game over if both players complete game"""
 			game_over += 1
 			if game_over == 2:
 				p1_data_queue.put("Game Over")
 				p2_data_queue.put("Game Over")
 				game_over = 0
 		else:
+			"""puts all other data in player 1 queue"""
 			p1_data_queue.put(data)
 
 	def connectionMade(self):
+		"""on ititial connection send data"""
 		p2_data_queue.get().addCallback(self.callback)
 		print 'New connection from', self.addr
 		global NUMBER_OF_PLAYERS
-		print NUMBER_OF_PLAYERS
+		"""if first player send missiles, else bombs"""
 		if NUMBER_OF_PLAYERS == 0:
 			NUMBER_OF_PLAYERS = 1
 			self.transport.write("Missiles")
@@ -57,10 +64,12 @@ class Player1Conn(Protocol):
 			self.transport.write("Bombs")
 
 	def connectionLost(self,reason):
+		"""if connection is lost, stop reactor"""
 		print 'Lost connection to', self.addr
 		reactor.stop()
 
 	def callback(self,data):
+		"""callback for p1"""
 		self.transport.write(data)
 		p2_data_queue.get().addCallback(self.callback)
 
@@ -69,6 +78,7 @@ class Player1ConnFactory(Factory):
 		pass
 
 	def buildProtocol(self, addr):
+		"""build plarer 1 connection"""
 		return Player1Conn(addr)
 
 #Player 2
@@ -79,6 +89,7 @@ class Player2Conn(Protocol):
 	def dataReceived(self,data):
 		global round_over,turn_over,game_over
 		if data == "Round Over":
+			"""only sends round over if both players complete round"""
 			round_over += 1
 			if round_over == 2:
 				print "hello"
@@ -86,21 +97,25 @@ class Player2Conn(Protocol):
 				p2_data_queue.put("Round Over")
 				round_over = 0
 		elif data == "Turn Over":
+			"""only sends turn over if both players complete turn"""
 			turn_over += 1
 			if turn_over == 2:
 				p1_data_queue.put("Turn Over")
 				p2_data_queue.put("Turn Over")
 				turn_over = 0
 		elif data == "Game Over":
+			"""only sends game over if both players complete game"""
 			game_over += 1
 			if game_over == 2:
 				p1_data_queue.put("Game Over")
 				p2_data_queue.put("Game Over")
 				game_over = 0
 		else:
+			"""adds other data to queue"""
 			p2_data_queue.put(data)
 
 	def connectionMade(self):
+		"""when connection is made, act like player one did"""
 		p1_data_queue.get().addCallback(self.callback)
 		print 'New connection from', self.addr
 		global NUMBER_OF_PLAYERS
@@ -113,10 +128,12 @@ class Player2Conn(Protocol):
 			self.transport.write("Bombs")
 
 	def connectionLost(self,reason):
+		"""stop rreactor if connection is lost"""
 		print 'Lost connection to', self.addr
 		reactor.stop()
 
 	def callback(self,data):
+		"""player 2 callback"""
 		self.transport.write(data)
 		p1_data_queue.get().addCallback(self.callback)
 
@@ -128,6 +145,7 @@ class Player2ConnFactory(Factory):
 		return Player2Conn(addr)
 
 if __name__ == '__main__':
+	"""listen on ports for clients and run reactor"""
 	reactor.listenTCP(PLAYER_1_PORT, Player1ConnFactory())
 	reactor.listenTCP(PLAYER_2_PORT, Player2ConnFactory())
 	reactor.run()
